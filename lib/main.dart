01cd0700739e842c6fb5f912c08ee67eb207a028
Antti Jurmu <antti.juhani.jurmu@gmail.com>
2026-08-31 13:44:46 +0300

    Cannot rotate app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kalorilaskuri/firebase_options.dart';
 import 'package:kalorilaskuri/notifiers.dart';
 import 'package:kalorilaskuri/pages/calories_page.dart';
@@ -13,6 +14,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
